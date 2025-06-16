08052eab8ccd96bab78d72040c8b135b0d5f6346
fleet2005 <100420vishalsrinivasan.gbkm@gmail.com>
2025-06-16 18:32:24 +0530

    whole parent container theme toggling

--- modeTogglerApp/src/App.jsx
+++ modeTogglerApp/src/App.jsx
@@ -1,13 +1,43 @@
+// import { useState } from 'react'
+// import './index.css'
+
+
+// const ModeToggler = () => {
+
+//   const [mode, setMode] = useState("light");
+    
+
+//   const toggleTheme = () => {
+//     setMode((prevMode)=>{
+//       if(prevMode == "light")
+//       {
+//         return "dark";
+//       }
+//       else
+//       {
+//         return "light";
+//       }
+//     })
+//   }
+
+
+//   return (
+//     <>
+//     <button className= {mode == "dark" ? "dark" : "light"} onClick={toggleTheme}>toggle theme</button>
+//     </>
+//   )
+// }
+
+
+
 import { useState } from 'react'
 import './index.css'
-
 
 const ModeToggler = () => {
 
   const [mode, setMode] = useState("light");
-    
 
-  const toggleTheme = () => {
+    const toggleTheme = () => {
     setMode((prevMode)=>{
       if(prevMode == "light")
       {
@@ -19,13 +49,14 @@ const ModeToggler = () => {
       }
     })
   }
+  
 
-
-  return (
-    <>
-    <button className= {mode == "dark" ? "dark" : "light"} onClick={toggleTheme}>toggle theme</button>
-    </>
-  )
+   return (
+      <div className= {mode == "dark" ? "dark" : "light"}>
+        <button onClick={toggleTheme}>toggle theme</button>
+        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, cum quidem numquam eaque cupiditate modi ab earum dolorum qui aspernatur tempora voluptate, doloribus nam, aperiam ipsum minus labore commodi quas.</h1>
+      </div>
+   )
 }
 
 
