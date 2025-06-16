37f256c43191a74ce2e3739741e6911d8dbe841c
fleet2005 <100420vishalsrinivasan.gbkm@gmail.com>
2025-06-16 21:34:31 +0530

    minor

--- modeTogglerApp/src/App.jsx
+++ modeTogglerApp/src/App.jsx
@@ -30,6 +30,41 @@
 
 
 
+// import { useState } from 'react'
+// import './index.css'
+
+// const ModeToggler = () => {
+
+//   const [mode, setMode] = useState("light");
+
+//     const toggleTheme = () => {
+//     setMode((prevMode)=>{
+//       if(prevMode == "light")
+//       {
+//         return "dark";
+//       }
+//       else
+//       {
+//         return "light";
+//       }
+//     })
+//   }
+  
+
+//    return (
+//       <div className= {mode == "dark" ? "dark" : "light"}>
+//         <button onClick={toggleTheme}>toggle theme</button>
+//         <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, cum quidem numquam eaque cupiditate modi ab earum dolorum qui aspernatur tempora voluptate, doloribus nam, aperiam ipsum minus labore commodi quas.</h1>
+//       </div>
+//    )
+// }
+
+
+// export default ModeToggler
+//do the above in the app.jsx to toggle all the components throughout the application
+
+//Yes — if you apply the theme class ("dark" or "light") at the top-level div inside App.jsx, it applies throughout the entire app — to every child component,
+
 import { useState } from 'react'
 import './index.css'
 
@@ -54,10 +89,11 @@ const ModeToggler = () => {
    return (
       <div className= {mode == "dark" ? "dark" : "light"}>
         <button onClick={toggleTheme}>toggle theme</button>
-        <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, cum quidem numquam eaque cupiditate modi ab earum dolorum qui aspernatur tempora voluptate, doloribus nam, aperiam ipsum minus labore commodi quas.</h1>
+        <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe sunt repudiandae magnam ea quis ratione culpa pariatur eveniet, magni velit, debitis officia laboriosam facilis, ad deleniti odio enim amet voluptates?</h1>
       </div>
    )
 }
+
 
 
 export default ModeToggler
